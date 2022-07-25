d620ee79deccf45e023bc2b7c36d9aff0486f1e5
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-25 20:44:35 -0300

    feat: requirement 26 was done.

---
+++ challenges/desafio26.js
@@ -0,0 +1,6 @@
+// 1. Crie uma query que faça a adição do valor contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40
+db.produtos.updateMany({
+  valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } },
+}, { $push: { tags: "contém sódio" } });
+// 2. Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.find({}, { nome: true, tags: true, _id: false });
