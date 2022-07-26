49a8fe57e3733657706928b2322ad682b090213a
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-25 21:15:43 -0300

    feat: requirement 31 was done.

--- challenges/desafio31.js
+++ challenges/desafio31.js
@@ -1,1 +1,2 @@
 db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } }, { nome: true, _id: false });
+// Source: https://stackoverflow.com/questions/4442453/mongodb-query-condition-on-comparing-2-fields
